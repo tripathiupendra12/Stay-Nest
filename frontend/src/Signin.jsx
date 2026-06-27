f362063285345351db8d4b2f0a99ec1253fab8a2
Upendra Tripathi <princetirpathi12@gmail.com>
2026-06-27 23:17:29 +0530

    Add

--- frontend/src/Signin.jsx
+++ frontend/src/Signin.jsx
@@ -113,7 +113,7 @@ const Signin = () => {
                   </div>
                 </div>
                 <Link
-                  href="#"
+                  to="#"
                   class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
                   Forgot password?
@@ -121,6 +121,7 @@ const Signin = () => {
               </div>
               <button
                 type="submit"
+                onClick={handleSubmit}
                 class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Sign in
